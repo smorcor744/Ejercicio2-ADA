7983cb4547786859218071909078a7abc252489a
Sergio <mcsergio@iesrafaelalberti.es>
2024-09-27 17:42:02 +0200

    echo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,28 +13,11 @@ import java.nio.file.Path
 
 * Una función que reciba una lista de diccionarios como la que devuelve la función anterior y devuelva dos listas, una con los alumnos aprobados y otra con los alumnos suspensos. Para aprobar el curso, la asistencia tiene que ser mayor o igual que el 75%, la nota de los exámenes parciales y de prácticas mayor o igual que 4 y la nota final mayor o igual que 5.*/
 
-fun main() {
-    val path = Path.of("src","main","resources","calificaciones.csv")
-    val classif = clasificaciones2(path)
-    println(classif)
-}
-fun notaFinal(clasif: MutableList<MutableMap<String,String>>){
-    clasif.forEach {
-        var notaFinal = 0.0
-        it.forEach { (clave,nota) ->
-            if(clave == "Parcial1"){
-                notaFinal += (nota.toDouble() * 0.30)
-            } else if(clave == "Parcial2"){
-                notaFinal += (nota.toDouble() * 0.30)
-            } else if(clave == "Practicas"){
-                notaFinal += (nota.toDouble() * 0.40)
-            }
-        }
-    }
-}
+
+// Una función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
 fun clasificaciones2(path: Path): MutableList<MutableMap<String,String>>{
 
-    var datos = mutableListOf<MutableMap<String,String>>()
+    val datos = mutableListOf<MutableMap<String,String>>()
     val br = Files.newBufferedReader(path)
 
     br.use {reader ->
@@ -54,6 +37,100 @@ fun clasificaciones2(path: Path): MutableList<MutableMap<String,String>>{
     return datos.sortedBy { it["Apellidos"] }.toMutableList()
 }
 
+
+// Una función que reciba una lista de diccionarios como la que devuelve la función anterior y añada a cada diccionario un nuevo par con la nota final del curso. El peso de cada parcial de teoría en la nota final es de un 30% mientras que el peso del examen de prácticas es de un 40%.
+fun notaFinal(clasif: MutableList<MutableMap<String, String>>): MutableList<MutableMap<String, String>> {
+    clasif.forEach { estudiante ->
+        var notaFinal = 0.0
+        val parcialesCompletados = mutableSetOf<String>()
+        var parcial1 = 0.0
+        var parcial2 = 0.0
+        var practica = 0.0
+
+
+        estudiante.forEach { (clave, nota) ->
+
+            val nota2 = nota.replace(",", ".").toDoubleOrNull() ?: 0.0
+            if (clave == "Parcial1" && nota2 <= 5.0){
+                parcial1 = nota2
+            } else if (clave == "Parcial2" && nota2 <= 5.0){
+                parcial2 = nota2
+            } else if (clave == "Practicas" && nota2 <= 5.0){
+                practica = nota2
+            }
+            when {
+                clave == "Parcial1" && nota2 >= 5.0 -> {
+                    notaFinal += (nota2 * 0.30)
+                    parcialesCompletados.add("Parcial1")
+                }
+                clave == "Parcial2" && nota2 >= 5.0 -> {
+                    notaFinal += (nota2 * 0.30)
+                    parcialesCompletados.add("Parcial2")
+                }
+                clave == "Practicas" && nota2 >= 5.0 -> {
+                    notaFinal += (nota2 * 0.40)
+                    parcialesCompletados.add("Practicas")
+                }
+
+                clave == "Ordinario1" && "Parcial1" !in parcialesCompletados -> {
+                    notaFinal += if (parcial1 > nota2) (parcial1 * 0.30) else (nota2 * 0.30)
+                }
+                clave == "Ordinario2" && "Parcial2" !in parcialesCompletados -> {
+                    notaFinal += if (parcial2 > nota2) (parcial2 * 0.30) else (nota2 * 0.30)
+                }
+                clave == "OrdinarioPracticas" && "Practicas" !in parcialesCompletados -> {
+                    notaFinal += if (practica > nota2) (practica * 0.30) else (nota2 * 0.40)
+                }
+            }
+        }
+
+        estudiante["Nota Final"] = String.format("%.2f", notaFinal)
+
+    }
+    println(clasif)
+
+    return clasif
+}
+
+
+// Una función que reciba una lista de diccionarios como la que devuelve la función anterior y devuelva dos listas, una con los alumnos aprobados y otra con los alumnos suspensos. Para aprobar el curso, la asistencia tiene que ser mayor o igual que el 75%, la nota de los exámenes parciales y de prácticas mayor o igual que 4 y la nota final mayor o igual que 5.*/
+fun passSusp(clasif: MutableList<MutableMap<String, String>>): Pair<MutableList<String>,MutableList<String>>{
+    val pass = mutableListOf<String>()
+    val suspense = mutableListOf<String>()
+    clasif.forEach { estudiante ->
+        val parcialesCompletados = mutableSetOf<String>()
+
+        estudiante.forEach { (clave, nota) ->
+            val nota2 = nota.replace(",", ".").replace("%", "").toDoubleOrNull() ?: 0.0
+            when {
+                clave == "Asistencia" && nota2 >= 75.0 -> {
+                    parcialesCompletados.add("Asistencia")
+                }
+                clave == "Nota Final" && nota2 >= 5.0 -> {
+                    parcialesCompletados.add("Nota Final")
+                }
+                clave == "Parcial1" && nota2 >= 4.0 -> {
+                    parcialesCompletados.add("Parcial1")
+                }
+                clave == "Parcial2" && nota2 >= 4.0 -> {
+                    parcialesCompletados.add("Parcial2")
+                }
+                clave == "Practicas" && nota2 >= 4.0 -> {
+                    parcialesCompletados.add("Practicas")
+                }
+                clave == "Ordinario1" && "Parcial1" !in parcialesCompletados && nota2 >= 5 -> parcialesCompletados.add("Parcial1")
+                clave == "Ordinario2" && "Parcial2" !in parcialesCompletados && nota2 >= 5 -> parcialesCompletados.add("Parcial2")
+                clave == "OrdinarioPracticas" && "Practicas" !in parcialesCompletados && nota2 >= 5 -> parcialesCompletados.add("Practicas")
+            }
+        }
+        if (parcialesCompletados.size >= 5) pass.add(estudiante["Apellidos"].toString()) else suspense.add(estudiante["Apellidos"].toString())
+    }
+
+    return Pair(pass, suspense)
+}
+
+
+// Esto es otra forma de sacar la lista filtrada por el apellido pero que no he utilizado para este ejecicio
 fun clasificaciones(path: Path): MutableList<MutableMap<String,MutableList<String>>>{
 
     var datos = mutableListOf<MutableMap<String,MutableList<String>>>()
@@ -79,4 +156,18 @@ fun clasificaciones(path: Path): MutableList<MutableMap<String,MutableList<Strin
         apellido
     }.toMutableList()
     return datos
+}
+
+fun main() {
+    val path = Path.of("C:\\2ºDAM\\ADA\\Ejerccicio2-ADA\\src\\main\\resources\\calificaciones.csv")
+    val classif = clasificaciones2(path)
+    val finalNota = notaFinal(classif)
+    val passSuspense = passSusp(finalNota)
+    val pass = passSuspense.first
+    val suspense = passSuspense.second
+
+    println("Han aprobado (${pass.size}): $pass")
+    println("Han suspendidos (${suspense.size}): $suspense")
+
+
 }
